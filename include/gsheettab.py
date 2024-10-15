adde430f57cfdf15711c30a87ab315fc8431f38d
Paulo Cabral <paulo.junior.cabral@gmail.com>
2024-10-15 18:33:09 -0300

    Adicionado utilização de Hooks do Airflow para busca dos dados da planilha do google sheets

--- include/gsheettab.py
+++ include/gsheettab.py
@@ -1,5 +1,5 @@
 from io import StringIO
-import requests
+from airflow.providers.http.hooks.http import HttpHook
 import pandas as pd
 from pandas import DataFrame
 
@@ -7,8 +7,21 @@ from pandas import DataFrame
 class GSheetTab():
 
     def __init__(self, id: str) -> None:
-        self.__sheet_url = f'https://docs.google.com/spreadsheets/d/{id}/export?format=csv'
+        self.http_hook = HttpHook(
+            method="GET",
+            http_conn_id="fraldas_otto_gsheet"
+        )
 
     def download_tab(self, tab_id: str) -> DataFrame:
-        url = f'{self.__sheet_url}&gid={tab_id}'
-        return pd.read_csv(url)
+        response = self.http_hook.run(
+            '/export', # Endpoint
+            data={
+                'format': 'csv',
+                'gid': tab_id
+            }
+        )
+        
+        if response.status_code == 200:
+            return pd.read_csv(StringIO(response.text))
+        else:
+            raise Exception(f"Erro ao baixar a planilha: {response.status_code}, {response.text}")
